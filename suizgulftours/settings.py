b9694fde862b25c12d7cd3fa6444f890ec989b8d
walidzakaria <walidpiano@yahoo.com>
2024-01-22 01:24:07 +0200

    allow cors

--- suizgulftours/settings.py
+++ suizgulftours/settings.py
@@ -34,6 +34,7 @@ DEBUG = config.get('App', 'DEBUG')
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
+
 CSRF_TRUSTED_ORIGINS = [
     'https://www.suezgulftours.com/',
     'https://suezgulftours.com/',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'info',
 ]
 
@@ -61,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'suizgulftours.urls'
